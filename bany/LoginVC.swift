5996e28ab31c840d22bc2af46fd263987ca8dc40
MoreNiceJay <morenicejay@gmail.com>
2015-09-23 23:29:01 -0400

    페이스북로그인이 실현되었다

---
+++ bany/LoginVC.swift
@@ -0,0 +1,63 @@
+//
+//  FirstViewController.swift
+//  bany
+//
+//  Created by Lee Janghyup on 9/23/15.
+//  Copyright © 2015 jay. All rights reserved.
+//
+
+import UIKit
+import Parse
+import FBSDKCoreKit
+import ParseFacebookUtilsV4
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func facebookSignUp(sender: AnyObject) {
+        
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(["public_profile", "email"], block: { (user:PFUser?, error:NSError?) -> Void in
+            
+            if(error != nil)
+            {
+            //display an alert message
+                let myAlert = UIAlertController(title: "Alert", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                
+                let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+                
+                myAlert.addAction(okAction)
+                self.presentViewController(myAlert, animated: true, completion: nil)
+                
+                return
+            }
+            
+            print(user)
+            print("Current user token = \(FBSDKAccessToken.currentAccessToken().tokenString)")
+            print("Current user id = \(FBSDKAccessToken.currentAccessToken().userID)")
+            
+            if(FBSDKAccessToken.currentAccessToken() != nil)
+            {
+                let loginNext = self.storyboard?.instantiateViewControllerWithIdentifier("LoginNextVC") as! LoginNextVC
+                
+                let loginNextNav = UINavigationController(rootViewController: loginNext)
+                
+                let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                
+                appDelegate.window?.rootViewController = loginNext
+            }
+            
+        })
+        
+    }
+
+}
+
---
+++ bany/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bany
+//
+//  Created by Lee Janghyup on 9/23/15.
+//  Copyright © 2015 jay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
