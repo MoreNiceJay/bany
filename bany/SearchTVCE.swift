685e8951bb423f6ff82e7e9e2bf98e7dc37f78c3
MoreNiceJay <morenicejay@gmail.com>
2015-10-04 01:46:52 -0400

    서치까지 완성하다니 ㄷ ㄷ ㄷ 이제 마무리를 하자꾸나

---
+++ bany/SearchTVCE.swift
@@ -0,0 +1,28 @@
+//
+//  SearchTVCE.swift
+//  bany
+//
+//  Created by Lee Janghyup on 10/3/15.
+//  Copyright © 2015 jay. All rights reserved.
+//
+
+import UIKit
+
+class SearchTVCE: UITableViewCell {
+
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ banyUITests/SearchTVC.swift
@@ -0,0 +1,219 @@
+//
+//  searchViewController.swift
+//  bany
+//
+//  Created by Lee Janghyup on 10/3/15.
+//  Copyright © 2015 jay. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SearchTVC: UIViewController, UITableViewDataSource ,UITableViewDelegate, UISearchBarDelegate {
+
+    @IBOutlet weak var myTable: UITableView!
+    @IBOutlet weak var mySearchBar: UISearchBar!
+    
+    
+    
+    
+    
+    var mainPhoto = [PFFile]()
+    var time = [String]()
+    var price = [String]()
+    var searchResults = [String]()
+    var objectArray = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.bringAllDatafromParse()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return searchResults.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! SearchTVCE
+        //cell.textLabel!.text = searchResults[indexPath.row]
+        
+        // 제목
+        cell.titleLabel.text = searchResults[indexPath.row]
+        // 시간
+        cell.timeLabel.text = String(time[indexPath.row])
+        // 가격
+        cell.priceLabel.text = price[indexPath.row]
+        //이미지
+        
+        mainPhoto[indexPath.row].getDataInBackgroundWithBlock { (imageData : NSData?, error : NSError?) -> Void in
+            let image = UIImage(data : imageData!)
+        
+            cell.mainImageView.image = image
+        }
+    
+    
+        
+        
+        return cell
+        
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        mySearchBar.resignFirstResponder()
+        
+        searchResults = []
+        mainPhoto = []
+        time = []
+        price = []
+       
+        
+        let firstNameQuery = PFQuery(className: "Posts")
+        
+        firstNameQuery.whereKey("tagText", containsString: searchBar.text)
+        
+        //firstNameQuery.whereKey("tagText", matchesRegex : "(?i)\(searchBar.text)")
+        
+        let lastNameQuery = PFQuery(className: "Posts")
+        lastNameQuery.whereKey("titleText", containsString: searchBar.text)
+        //lastNameQuery.whereKey("titleText", matchesRegex : "(?i)\(searchBar.text)")
+
+        let query = PFQuery.orQueryWithSubqueries([firstNameQuery, lastNameQuery])
+        
+        query.findObjectsInBackgroundWithBlock { (results, error) -> Void in
+            if error != nil{
+                
+                self.alert("Alert", message : (error?.localizedDescription)!)
+                
+                return
+            }
+            if let objects = results as [PFObject]? {
+                
+                self.searchResults.removeAll(keepCapacity: false)
+                
+                for object in objects {
+                    let tagText = object.objectForKey("tagText") as! String
+                    let titleText = object.objectForKey("titleText") as! String
+                    
+                    
+                    
+                    
+                                        //시간
+                    let dateFormatter:NSDateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+                    self.time.append(dateFormatter.stringFromDate(object.createdAt!))
+                    self.price.append(object["priceText"] as! String)
+                    
+                    self.objectArray.append((object.objectId)! as String!)
+                    
+                    //self.viewed.append(post["view"] as! Int)
+                    //  self.saved.append(post["like"] as! Int)
+                    
+                    self.mainPhoto.append(object["imageFile"] as! PFFile)
+                    //self.profilePhoto.append(post["profilePhoto"] as! PFFile)
+
+                    
+                    
+                    
+                    
+                    
+                    let fullResult = tagText + "" + titleText
+                    
+                    self.searchResults.append(fullResult)
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.myTable.reloadData()
+                    self.mySearchBar.resignFirstResponder()
+                })
+                
+            }
+        }
+        
+    }
+    
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        mySearchBar.resignFirstResponder()
+        mySearchBar.text = ""
+    }
+    
+    func alert(title : String, message : String) {
+        
+        let myAlert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
+
+    @IBAction func refreshButtonTapped(sender: AnyObject) {
+        
+        mySearchBar.resignFirstResponder()
+        mySearchBar.text = ""
+        searchResults.removeAll(keepCapacity: false)
+        myTable.reloadData()
+        
+    }
+    
+    func bringAllDatafromParse() {
+        //activityIndicatorOn()
+        
+        let query = PFQuery(className: "Posts")
+        
+        query.findObjectsInBackgroundWithBlock { (posts, error) -> Void in
+            if error == nil {
+                
+                for post in posts! {
+                    
+                    
+                    
+                    
+                    
+                    
+                    self.searchResults.append(post["titleText"] as! String)
+                    //시간
+                    let dateFormatter:NSDateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+                    self.time.append(dateFormatter.stringFromDate(post.createdAt!))
+                    self.price.append(post["priceText"] as! String)
+                    
+                    self.objectArray.append((post.objectId)! as String!)
+                    
+                    //self.viewed.append(post["view"] as! Int)
+                    //  self.saved.append(post["like"] as! Int)
+                    
+                    self.mainPhoto.append(post["imageFile"] as! PFFile)
+                    //self.profilePhoto.append(post["profilePhoto"] as! PFFile)
+                    
+                    self.myTable.reloadData()
+                }
+            }
+        }
+    
+}
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if (segue.identifier == "searchToDetail") {
+            
+            
+            let selectedRowIndex = self.myTable.indexPathForSelectedRow
+            let destViewController : DetailVC = segue.destinationViewController as! DetailVC
+            destViewController.parentObjectID = objectArray[(selectedRowIndex?.row)!]
+
+            
+        }
+    }
+    
+    
+    
+}
