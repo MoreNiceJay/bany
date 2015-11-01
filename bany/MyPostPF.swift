fd8deea6f2995fc2665b536392727a56b0c5072d
MoreNiceJay <morenicejay@gmail.com>
2015-11-01 06:42:26 -0500

    마이포스트 리턴 셀 에러

---
+++ bany/MyPostPF.swift
@@ -0,0 +1,104 @@
+//
+//  MyPostPF.swift
+//  bany
+//
+//  Created by Lee Janghyup on 11/1/15.
+//  Copyright © 2015 jay. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class MyPostPF : PFQueryTableViewController {
+    
+    override init(style: UITableViewStyle, className: String?) {
+        super.init(style: style, className: className)
+        parseClassName = "Posts"
+        pullToRefreshEnabled = true
+        paginationEnabled = true
+        objectsPerPage = 20
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        parseClassName = "Posts"
+        pullToRefreshEnabled = true
+        paginationEnabled = true
+        objectsPerPage = 20
+    }
+    
+    override func queryForTable() -> PFQuery {
+        let query = PFQuery(className: self.parseClassName!)
+        query.whereKey("uploader", equalTo: (PFUser.currentUser()?.objectId)!)
+
+        // If no objects are loaded in memory, we look to the cache first to fill the table
+        // and then subsequently do a query against the network.
+        //if self.objects!.count == 0 {
+        //  query.cachePolicy = .CacheThenNetwork
+        // }
+        
+        query.orderByDescending("createdAt")
+        
+        return query
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
+        let cellIdentifier = "myPostCell"
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? MyPostTVCE
+        
+        // Show sold label or not
+        cell!.soldLabel.hidden = !(object!["sold"] as! Bool)
+        
+        
+        // title Label of post
+        cell!.titleLabel.text = " " + (object!["titleText"] as! String)
+        
+        // price label
+        let price = (object!["priceText"] as! String)
+        cell!.priceLabel.text = " $\(price)"
+        
+        
+        
+        // time label for posts
+        let dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MM /dd /yy"
+        cell!.timeLabel.text = (dateFormatter.stringFromDate(object!.createdAt!))
+        
+        
+        //   main Image for post
+        let mainImages = object!["front_image"] as! PFFile
+        mainImages.getDataInBackgroundWithBlock { (imageData, error) -> Void in
+            let image = UIImage(data: imageData!)
+            cell?.mainImageView.image = image
+        }
+        
+        
+        //    if cell == nil {
+        //            cell = PFTableViewCell(style: .Subtitle, reuseIdentifier: cellIdentifier)
+        //        }
+        
+        //        // Configure the cell to show todo item with a priority at the bottom
+        //        if let object = object {
+        //            cell!.textLabel?.text = object["titleText"] as? String
+        //
+        //        }
+        
+        return cell
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let detailViewController = segue.destinationViewController
+            as! DetailVC
+        if segue.identifier == "mainToDetail"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            detailViewController.object = (self.objects![indexPath!.row] as! PFObject)
+            print((self.objects![indexPath!.row] as! PFObject))
+        }
+        
+        
+    }
+    
+    
+}
